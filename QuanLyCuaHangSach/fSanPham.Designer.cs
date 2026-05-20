0011c60ca361b634ea77572efcab3258b41035c0
dlphuog <pdang6849@gmail.com>
2026-05-20 16:21:10 +0700

    dang design fSanPham

--- QuanLyCuaHangSach/fSanPham.Designer.cs
+++ QuanLyCuaHangSach/fSanPham.Designer.cs
@@ -28,12 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.Transparent;
+            this.pnlTop.Controls.Add(this.btnThem);
+            this.pnlTop.Location = new System.Drawing.Point(20, 20);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(860, 70);
+            this.pnlTop.TabIndex = 0;
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.Color.White;
+            this.btnThem.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThem.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(29, 3);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(90, 50);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "+ Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            // 
+            // fSanPham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.pnlTop);
+            this.Name = "fSanPham";
             this.Text = "fSanPham";
+            this.pnlTop.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Button btnThem;
     }
 }
